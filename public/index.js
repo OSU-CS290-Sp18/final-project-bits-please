ff8c76e9fd063e3fd72e66f3c55e303fa0372c77
vbluestone <bluestov@oregonstate.edu>
2018-06-07 18:00:10 -0700

    basic js for add photo popup to test

--- public/index.js
+++ public/index.js
@@ -13,3 +13,16 @@ var addItemButton = document.getElementById('add-item-button');
 if (addItemButton) {
   addItemButton.addEventListener('click', showAddItemModal);
 }
+function hideAddItemModal() {
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var addItemModal = document.getElementById('add-item-modal');
+
+  // Hide the modal and its backdrop.
+  modalBackdrop.classList.add('hidden');
+  addItemModal.classList.add('hidden');
+}
+var modalCloseButton = document.querySelector('#add-item-modal .modal-close-button');
+if (modalCloseButton) {
+  modalCloseButton.addEventListener('click', hideAddItemModal);
+}
