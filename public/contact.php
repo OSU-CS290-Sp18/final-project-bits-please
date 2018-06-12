e5d60108e5f58bddd8ff71c4401485a682d71348
ErinVillasenor <38110750+ErinVillasenor@users.noreply.github.com>
2018-06-12 10:20:02 -0700

    General updated
    Removed required fields

--- public/contact.php
+++ public/contact.php
@@ -1,21 +1,23 @@
+<link rel= "stylesheet" href = "contact.css" type = "text/css">
+
 <div class="container">  
   <form id="contact" action="" method="post">
     <h3>Quick Contact</h3>
     <h4>Contact us today, and get reply with in 24 hours!</h4>
     <fieldset>
-      <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+      <input placeholder="Your name" type="text" tabindex="1" autofocus>
     </fieldset>
     <fieldset>
-      <input placeholder="Your Email Address" type="email" tabindex="2" required>
+      <input placeholder="Your Email Address" type="text" tabindex="2" >
     </fieldset>
     <fieldset>
-      <input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+      <input placeholder="Your Phone Number" type="text" tabindex="3" >
     </fieldset>
     <fieldset>
-      <input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required>
+      <input placeholder="Your Web Site starts with http://" type="text" tabindex="4" >
     </fieldset>
     <fieldset>
-      <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+      <textarea placeholder="Type your Message Here...." type = "text" tabindex="5" ></textarea>
     </fieldset>
     <fieldset>
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
