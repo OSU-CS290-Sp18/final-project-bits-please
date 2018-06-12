97efde15f1b56ea297306fafbe615513b46e4594
ErinVillasenor <38110750+ErinVillasenor@users.noreply.github.com>
2018-06-12 11:26:48 -0700

    Update contact_process.php

--- public/contact_process.php
+++ public/contact_process.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           $message_body .=  "$key: $value\n";
       }
       
-      $to = 'vladi@clevertechie.com';
+      $to = 'villaser@oregonstate.edu';
       $subject = 'Contact Form Submit';
       if (mail($to, $subject, $message)){
           $success = "Message sent, thank you for contacting us!";
