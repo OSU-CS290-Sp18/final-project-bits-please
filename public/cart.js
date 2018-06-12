1ec2e7d6c36d199c98e85bc155a31ab52cbbb486
ErinVillasenor <38110750+ErinVillasenor@users.noreply.github.com>
2018-06-12 14:11:38 -0700

    Create cart.js

---
+++ public/cart.js
@@ -0,0 +1,70 @@
+var cart = [];
+
+var Item = function (name, price, count) {
+this.name = name
+this.price = price
+this.count = count
+};
+
+function addItemToCart(name, price, count)
+{
+  for (var i in cart) {
+    if (cart[i].name === name)  {
+    
+    cart[i].count += count;
+    return;
+  }
+  }
+  var item = new Item(name, price, count);
+  cart.push(item);
+    saveCart();
+
+}
+
+function removeItemFromCart(name) {
+  for ( var i in cart) {
+    if(cart[i].name === name) {
+     cart[i].count--;
+     if (cart[i].count === 0 {
+      cart.splice(i,1);
+     }
+     break;
+  }
+  
+  }
+  saveCart();
+}
+
+function removeItemFromCartAll(name) {
+  for (var i in cart) {
+  if  (cart[i].name === name) {
+      cart.splice(i, 1);
+      break;
+      }
+     }
+     saveCart();
+    }
+}
+
+function clearCart()  {
+ cart = [];
+ saveCart();
+ }
+ 
+ function countCart() {
+    var totalCount = 0;
+    for (var i in cart)  {
+    totalCount += cart[i].count;
+    }
+    return totalCount;
+ }
+ 
+ function costCart() {
+  var totalCost = 0;
+  for (var i in cart) {
+    totalCost += cart[i].price;
+    }
+    return totalCost;
+  }
+ 
+ }
