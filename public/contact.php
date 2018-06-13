9e87048a3df34ff2678c26ce4362aa09f3944f2f
ErinVillasenor <38110750+ErinVillasenor@users.noreply.github.com>
2018-06-12 21:34:10 -0700

    Create contact.php

---
+++ public/contact.php
@@ -0,0 +1,37 @@
+<?php include ('contact_process.php'); ?>
+<link rel= "stylesheet" href = "contact.css" type = "text/css">
+
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <form id ="contact" action ="<?= $_SERVER['PHP_SELF']; ?>" method = "post"
+    <h3>Quick Contact</h3>
+    <h4>Contact us today, and get a reply within 24 hours!</h4>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" name = "name" value = "<?= $name ?> autofocus>
+      <span class = "error"><?= $name_error?></span>  
+  </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="text" tabindex="2" name = "email" value = "<?= $email ?>>
+            <span class = "error"><?= $email_error?></span>  
+
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number" type="text" tabindex="3" name = "phone" value = "<?= $phone ?>>
+      <span class = "error"><?= $phone_error?></span>  
+  </fieldset>
+    <fieldset>
+      <input placeholder="Your Web Site starts with http://" type="text" tabindex="4" name = "url" value = "<?= $url ?>>
+      <span class = "error"><?= $url_error?></span>  
+
+  </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your Message Here...." type = "text" tabindex="5" name = "message" value = "<?= $message ?>></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+        <div class = "success"><?= $success; ?></div>  
+  </form>
+ 
+  
+</div>
