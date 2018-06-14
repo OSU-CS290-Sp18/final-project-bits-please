b75a00f6a26c3ef3d1a5a3da344f852c45f358a3
VBluestone <35310763+VBluestone@users.noreply.github.com>
2018-06-14 13:07:52 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,20 @@
+/***HOME PAGE*****/
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
 
 /*ADD ITEM POPUP WINDOW JS*/
 //show add item modal
@@ -140,19 +157,3 @@ searchButton.addEventListener("click", function(event){
     
     /*Work In Progress*/
 });
-
-/***HOME PAGE*****/
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
